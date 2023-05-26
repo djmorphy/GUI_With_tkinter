7aca463f74012f16e515a6691bb88d36cf8cb98d
djmorphy <djmorphy@gmail.com>
2023-05-26 18:38:07 +0200

    calculator adding, deleting numbers,

--- calculator.py
+++ calculator.py
@@ -7,11 +7,25 @@ e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 # e.insert(0, "")
 
 def button_click(number):
-    #e.delete(0, END) #kireseteli az inputot
-    e.insert(0, number)
+    current = e.get()
+    e.delete(0, END)
+    e.insert(0, str(current) + str(number))
 
 
+def button_clear():
+    e.delete(0, END)
 
+def button_add():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    e.delete(0, END)
+
+
+def button_equal():
+    second_number = e.get()
+    e.delete(0, END)
+    e.insert(0, f_num + int(second_number))
 
 
 #define Buttons
@@ -25,9 +39,9 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-button_add = Button(root, text="+", padx=39, pady=20, command=lambda: button_click())
-button_equal = Button(root, text="=", padx=91, pady=20, command=lambda: button_click())
-button_clear = Button(root, text="Clear", padx=79, pady=20, command=lambda: button_click())
+button_add = Button(root, text="+", padx=39, pady=20, command= button_add)
+button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
+button_clear = Button(root, text="Clear", padx=79, pady=20, command= button_clear)
 
 #Put the buttons on the screen
 
